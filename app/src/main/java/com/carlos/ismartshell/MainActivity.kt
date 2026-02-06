041304475ac9de38bcc76e1668bfcc51a1503619
carlos22101 <233429@ids.upchiapas.edu.mx>
2026-02-05 23:14:46 -0600

    Initial commit

---
+++ app/src/main/java/com/carlos/ismartshell/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.carlos.ismartshell
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+import com.carlos.ismartshell.ui.theme.ISmartshellTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ISmartshellTheme {
+
+                }
+            }
+        }
+    }
+
+
